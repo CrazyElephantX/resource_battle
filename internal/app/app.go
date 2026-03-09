f450922de79dd2f93394072ed1d6e9ce0d5474cb
CrazyElephant_X <79636902266@ya.ru>
2026-03-09 10:47:35 +0300

    каркас сервиса, для подсчета баллов во время игры

---
+++ internal/app/app.go
@@ -0,0 +1,32 @@
+package app
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+func New(pool *pgxpool.Pool) http.Handler {
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		_, _ = w.Write([]byte("ok"))
+	})
+
+	// TODO: dashboard/admin routes.
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		_, _ = w.Write([]byte("resource_battle: TODO UI"))
+	})
+
+	_ = pool // will be used by handlers
+	return r
+}
+
